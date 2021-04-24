a789c28c63de54279c9af02ed0f8b943e90fb20d
eqs <murashige.satoshi.mi1@is.naist.jp>
2021-04-24 16:55:09 +0900

    v0.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name="yacho",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/eqs/yacho",
     license='MIT',
 
--- yacho/__init__.py
+++ yacho/__init__.py
@@ -1,5 +1,5 @@
 """yacho - yacho"""
 
-__version__ = '0.0.0'
+__version__ = '0.0.2'
 __author__ = 'eqs <murashige.satoshi.mi1 [at] gmail.com>'
 __all__ = []
